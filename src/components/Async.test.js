943cf050984284fdb13e1747fca3a91bd2f64247
rkeerthick <rkeerthick1@gmail.com>
2023-12-13 10:55:01 +0530

    final commit

--- src/components/Async.test.js
+++ src/components/Async.test.js
@@ -1,10 +1,14 @@
-import { render, screen } from '@testing-library/react';
-import Async from '../components/Async'
+import { render, screen } from "@testing-library/react";
+import Async from "../components/Async";
 
 describe("async test suite", () => {
-    test("render post if request", async () => {
-        render(<Async />)
-        const listItemElements = await screen.findAllByRole('listitem')
-        expect(listItemElements).not.toHaveLength(0)
-    })
-})
+  test("render post if request", async () => {
+    window.fetch = jest.fn();
+    window.fetch.mockResolvedValueOnce({
+      json: async () => [{ id: 1, title: "hi" }],
+    });
+    render(<Async />);
+    const listItemElements = await screen.findAllByRole("listitem");
+    expect(listItemElements).not.toHaveLength(0);
+  });
+});
